ffc1d07c8a974204e80223d9db5b63a75fc0336f
gaocangxiong <Lensgcx@163.com>
2026-09-05 21:57:49 +0800

    组件引用注册调整
    组件引用注册调整

---
+++ apps/hrs-web/src/components/common/Card/index.ts
@@ -0,0 +1,4 @@
+export { default as AnimCard } from './AnimCard';
+export { NewsCard } from './newsCard';
+export type { AnimCardProps } from './AnimCard';
+export type { NewsCardProps } from './newsCard';
